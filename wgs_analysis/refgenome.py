b69cee1d1cd3ab04e8802e844c85d4d9c3911e7f
Andrew McPherson <andrew.mcpherson@gmail.com>
2020-04-19 19:46:21 -0400

    feature: additional refgenome fields

--- wgs_analysis/refgenome.py
+++ wgs_analysis/refgenome.py
@@ -12,17 +12,25 @@ def read_chromosome_lengths(genome_fasta_index):
 class RefGenomeInfo(object):
     def __init__(self, version):
         if version == 'hg19':
-            self.chromosomes = [str(a) for a in range(1, 23)] + ['X']
+            self.chromosomes = [str(a) for a in range(1, 23)] + ['X', 'Y']
 
             genome_fasta_index = pkg_resources.resource_filename('wgs_analysis', 'data/GRCh37-lite.fa.fai')
 
             self.chromosome_lengths = pd.Series(read_chromosome_lengths(genome_fasta_index)).reindex(self.chromosomes).astype(int)
+            self.chromosome_lengths.index.name = 'chr'
 
             self.chromosome_end = np.cumsum(self.chromosome_lengths)
             self.chromosome_start = self.chromosome_end.shift(1)
             self.chromosome_start[0] = 0
             self.chromosome_start = self.chromosome_start.astype(int)
             self.chromosome_mid = (self.chromosome_start + self.chromosome_end) / 2.
+
+            self.chromosome_info = pd.DataFrame({
+                'chromosome_length': self.chromosome_lengths,
+                'chromosome_end': self.chromosome_end,
+                'chromosome_start': self.chromosome_start,
+                'chromosome_mid': self.chromosome_mid,
+            }).reset_index()
 
 info = None
 
