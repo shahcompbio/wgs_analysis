93f9d2cee0bc924c222cabb1e08e50491374dbad
Andrew McPherson <andrew.mcpherson@gmail.com>
2022-07-14 12:47:58 -0400

    versioneer

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import find_packages, setup
+import versioneer
+
 
 setup(
     name='wgs_analysis',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     description='Whole genome sequencing analysis library',
     packages=find_packages(),
     package_data={'':['data/*']},
