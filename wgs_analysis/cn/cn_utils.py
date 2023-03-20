29a6ce0c5d94a35d4751e1249199102432d4a585
Seongmin Choi <soymintc@gmail.com>
2023-03-20 12:55:39 -0400

    fix script name for import purposes, fix p-arm patching bug

--- wgs_analysis/cn/cn_utils.py
+++ wgs_analysis/cn/cn_utils.py
@@ -859,6 +859,7 @@ class CopyNumberChangeData:
         df = proc_cn_changes(self.cn_changes[group])
         ideogram, gene_chroms = get_ideogram_and_chroms(self.gene_ranges, self.chroms)
         arms = get_chrom_arm_coords(self.centromere_table, per_chromosome=True)
+        arms.loc[:, ['start', 'end', 'length']] /= self.factor
 
         cohorts_tag = ' + '.join(self.cohorts)
         if group != 'merged': cohorts_tag = f'{cohorts_tag} : {group}'
@@ -906,19 +907,19 @@ class CopyNumberChangeData:
             plt.savefig(out_path)
 
         
-logging.basicConfig(level = logging.INFO)
-gene_list_path = '/juno/work/shah/users/chois7/tickets/cohort-cn-qc/resources/gene_list.txt'
-for cohorts in (['SPECTRUM'], ['Metacohort'], ['SPECTRUM', 'Metacohort']):
-    cn = CopyNumberChangeData(gene_list=gene_list_path, cohorts=cohorts)
-    cohort_symbol = '_'.join(cn.cohorts)
-    for signature in cn.signature_counts:
-        logging.info(f'processing cohorts:{cohorts} signature:{signature}')
-        if cn.signature_counts[signature] > 5:
-            logging.info(f'plotting cohorts:{cohorts} signature:{signature}')
-            cn.plot_pan_chrom_cn(group=signature, out_path=f'{cohort_symbol}.{signature}.pdf')
-            cn.plot_per_chrom_cn(group=signature, out_path=f'{cohort_symbol}.{signature}.per-chrom.pdf')
-
-    gene_cn = cn.get_gene_cn_counts()
-    results = evaluate_enrichment(cn.signatures, cn.signature_counts, cn.gene_list, 
-            cn.sample_counts, padj_cutoff=0.1)
-    results.to_csv(f'enrichment.{cohort_symbol}.tsv', sep='\t', index=False)
+#logging.basicConfig(level = logging.INFO)
+#gene_list_path = '/juno/work/shah/users/chois7/tickets/cohort-cn-qc/resources/gene_list.txt'
+#for cohorts in (['SPECTRUM'], ['Metacohort'], ['SPECTRUM', 'Metacohort']):
+#    cn = CopyNumberChangeData(gene_list=gene_list_path, cohorts=cohorts)
+#    cohort_symbol = '_'.join(cn.cohorts)
+#    for signature in cn.signature_counts:
+#        logging.info(f'processing cohorts:{cohorts} signature:{signature}')
+#        if cn.signature_counts[signature] > 5:
+#            logging.info(f'plotting cohorts:{cohorts} signature:{signature}')
+#            cn.plot_pan_chrom_cn(group=signature, out_path=f'{cohort_symbol}.{signature}.pdf')
+#            cn.plot_per_chrom_cn(group=signature, out_path=f'{cohort_symbol}.{signature}.per-chrom.pdf')
+#
+#    gene_cn = cn.get_gene_cn_counts()
+#    results = evaluate_enrichment(cn.signatures, cn.signature_counts, cn.gene_list, 
+#            cn.sample_counts, padj_cutoff=0.1)
+#    results.to_csv(f'enrichment.{cohort_symbol}.tsv', sep='\t', index=False)
