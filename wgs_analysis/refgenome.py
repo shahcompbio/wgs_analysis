4e9c9a9fd1ec3a50266b35c3a2e78abb63625ebf
Andrew McPherson <andrew.mcpherson@gmail.com>
2023-07-03 18:05:17 -0400

    feature: grch38 ref genome

--- wgs_analysis/refgenome.py
+++ wgs_analysis/refgenome.py
@@ -32,6 +32,27 @@ class RefGenomeInfo(object):
                 'chromosome_mid': self.chromosome_mid,
             }).reset_index()
 
+        elif version == 'grch38':
+            self.chromosomes = [f'chr{a}' for a in range(1, 23)] + ['chrX', 'chrY']
+
+            genome_fasta_index = pkg_resources.resource_filename('wgs_analysis', 'data/GRCh38.fa.fai')
+
+            self.chromosome_lengths = pd.Series(read_chromosome_lengths(genome_fasta_index)).reindex(self.chromosomes).astype(int)
+            self.chromosome_lengths.index.name = 'chr'
+
+            self.chromosome_end = np.cumsum(self.chromosome_lengths)
+            self.chromosome_start = self.chromosome_end.shift(1)
+            self.chromosome_start[0] = 0
+            self.chromosome_start = self.chromosome_start.astype(int)
+            self.chromosome_mid = (self.chromosome_start + self.chromosome_end) / 2.
+
+            self.chromosome_info = pd.DataFrame({
+                'chromosome_length': self.chromosome_lengths,
+                'chromosome_end': self.chromosome_end,
+                'chromosome_start': self.chromosome_start,
+                'chromosome_mid': self.chromosome_mid,
+            }).reset_index()
+
 info = None
 
 
