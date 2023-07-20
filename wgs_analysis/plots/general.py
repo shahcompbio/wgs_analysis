7f7ad74191f0c9cc569e5c92298ff20323927c1b
Seongmin Choi <soymintc@gmail.com>
2023-07-20 14:53:34 -0400

    add gene plotting

--- wgs_analysis/plots/general.py
+++ wgs_analysis/plots/general.py
@@ -3,10 +3,12 @@
 
 '''
 
+import pysam
 import matplotlib
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from collections import defaultdict
 from .. import refgenome
 
 # import wgs_analysis.helpers as helpers
@@ -21,6 +23,194 @@ from .. import refgenome
 
 #     fig.savefig(legend_filename, bbox_inches='tight')
 
+
+def convert_tupleproxy_to_pyranges(exon):
+    """ Convert TabixFile 'exon' (TupleProxy) to pyranges-like fields
+    - exon: TupleProxy (result from iterating TabixFile
+    
+    Returns: pyranges-like fields [list]
+    """
+    assert len(exon) == 9, f'exon (length:{len(exon)}): {tuple(exon)}'
+    exon_field = exon[:8]
+    info = exon[8].rstrip(';')
+    annots = info.split('; ')
+    annot_map = {v.split(' ')[0]: v.split(' ')[1][1:-1] for v in annots} # [1:-1] hack to quick-remove ""
+
+    annot_cols = ['gene_id', 'gene_version', 'gene_name', 'gene_source',
+        'gene_biotype', 'transcript_id', 'transcript_version',
+        'transcript_name', 'transcript_source', 'transcript_biotype', 'tag',
+        'transcript_support_level', 'exon_number', 'exon_id', 'exon_version',
+        'protein_id', 'protein_version', 'ccds_id']
+
+    annot_field = [annot_map[k] if k in annot_map else np.nan for k in annot_cols]
+    field = exon_field + annot_field
+    return field
+
+def parse_gtf_region(gtf, region):
+    """ Convert pysam TabixFile fetch results to quasi-pyranges DataFrame
+    - gtf: pysam.TabixFile
+    - region: tuple of (chromosome[str], start[int], end[int])
+    
+    Returns: pd.DataFrame
+    """
+    pyranges_cols = [
+        'Chromosome', 'Source', 'Feature', 'Start', 'End', 'Score', 'Strand',
+        'Frame', 'gene_id', 'gene_version', 'gene_name', 'gene_source',
+        'gene_biotype', 'transcript_id', 'transcript_version',
+        'transcript_name', 'transcript_source', 'transcript_biotype', 'tag',
+        'transcript_support_level', 'exon_number', 'exon_id', 'exon_version',
+        'protein_id', 'protein_version', 'ccds_id'
+    ]
+    int_cols = ['Start', 'End',]
+    df = pd.DataFrame(columns=pyranges_cols)
+    for exon in gtf.fetch(*region, parser=pysam.asTuple()):
+        field = convert_tupleproxy_to_pyranges(exon)
+        df.loc[df.shape[0]] = field
+    df[int_cols] = df[int_cols].astype(int)
+    return df
+
+def get_gene_repr_exons(gtf, gene, lenient=False):
+    """ Get representative exons from given gene name
+    - gtf: pyranges-like DataFrame from parsing GTF
+    - gene: gene symbol
+
+    Returns: DataFrame of exons of the representative isoform of the give gene
+    """
+    transcript = get_repr_transcript_id(gtf, gene, lenient=lenient)
+    exons = get_transcript_exons(gtf, transcript)
+    return exons
+
+def get_transcript_exons(gtf, transcript_id):
+    exons = gtf[
+        (gtf['transcript_id']==transcript_id) &
+        (gtf['Feature'].isin(['exon', 'CDS']))
+    ]
+    return exons
+
+def get_repr_transcript_id(gtf, gene_name, lenient=False):
+    """ Select the representative transcript isoform of a gene symbol
+    - gtf: pyranges-like DataFrame from parsing GTF
+    - gene: gene symbol
+
+    Returns: transcript_id[str]
+    """
+    if not lenient:
+        transcripts = (
+            gtf
+            .query(f"gene_name == '{gene_name}'")
+            .query("Feature == 'transcript'")
+            .query("transcript_biotype == 'protein_coding'")
+            .query("transcript_support_level == '1'")
+        ).copy()
+    else:
+        transcripts = (
+            gtf
+            .query(f"gene_name == '{gene_name}'")
+            .query("Feature == 'transcript'")
+        ).copy()
+    transcript_id = None
+    if transcripts.shape[0] > 0:
+        transcripts['length'] = transcripts['End'] - transcripts['Start']
+        transcript = transcripts.sort_values(by=['length'], ascending=False).iloc[0]
+        transcript_id = transcript['transcript_id']
+    return transcript_id
+
+def add_gene_annotations(ax, gene_exons, return_offset=False):
+    """ Plot UCSC-like gene annotation to Axes
+    - ax: pyplot.Axes
+    - gene_exons[dict]: {gene_symbol: pyranges-like GTF DataFrame}
+    - return_offset: return gene -> offset map [dict]
+
+    Returns: gene2offset[dict] (optional)
+    """
+    gene2offset = defaultdict(int)
+    intervals = []
+    for gene, exons in enumerate(gene_exons.items()):
+        start_min = exons['Start'].min()
+        end_max = exons['End'].max()
+        if not np.isnan(start_min) and not np.isnan(end_max):
+            interval = (start_min, end_max, gene)
+            intervals.append(interval)
+    intervals.sort(key=lambda x: (x[0], x[1]))
+    n_intervals = len(intervals)
+    margin = 5e4
+    for i in range(n_intervals):
+        for j in range(i+1, n_intervals):
+            start_i, end_i, gene_i = intervals[i]
+            start_j, end_j, gene_j = intervals[j]
+            if is_overlap((start_i-margin, end_i+margin), (start_j-margin, end_j+margin)):
+                if gene2offset[gene_i] == gene2offset[gene_j]:
+                    gene2offset[gene_j] += 1
+        
+    for gene, exons in gene_exons.items():
+        if exons.shape[0] == 0: continue # filter out if none
+        strands = exons['Strand'].unique()
+        assert len(strands) == 1, strands
+        strand = strands[0]
+        transcript_s, transcript_e = exons['Start'].min(), exons['End'].max()
+        offset = gene2offset[gene]
+        for i, (_, row) in enumerate(exons.iterrows()):
+            add_annot = (i==0)
+            _add_transcript_rectangles(ax, row, transcript_s, transcript_e, offset, strand, add_annot=add_annot) # add rectangles and a line
+
+    if return_offset:
+        return gene2offset
+
+def _add_quiver(ax, x_quiver_starts, y_quiver_starts, x_quiver_directs, y_quiver_directs, box_color='#090A76'):
+    ax.quiver(
+        x_quiver_starts, y_quiver_starts, x_quiver_directs, y_quiver_directs,
+        color=box_color,
+        width=0.005, 
+        headwidth=30, 
+        headaxislength=5,
+        headlength=17, 
+        pivot='tip',
+        scale=100,
+    )
+
+def _add_transcript_rectangles(ax, row, transcript_s, transcript_e, y_offset, strand, add_annot=False):
+    box_color = '#090A76'
+    strand_color = 'blue'
+    x_direct, y_direct = -1, 0
+    if strand == '+': 
+        x_direct = 1
+        strand_color = 'red'
+    x_min, x_max = ax.get_xlim()
+    y_offset_factor = 3
+    y_offset *= y_offset_factor
+    exon_s, exon_e = row['Start'], row['End']
+    feature = row['Feature']
+    rect_y, rect_height = 1+y_offset, 1
+    transcript_line_y = rect_y + 0.5 * rect_height
+    text_y = rect_y - rect_height + 3
+    if feature == 'CDS':
+        rect_y = 0.5 + y_offset
+        rect_height = 2
+    exon_len = exon_e - exon_s
+    rect_x = exon_s #max(exon_s, start)
+    
+    # for each exon / CDS
+    ax.add_patch(matplotlib.patches.Rectangle((rect_x, rect_y), exon_len, rect_height, 
+                                              linewidth=1, edgecolor=strand_color, color=box_color, zorder=1))
+    if add_annot: 
+        quiver_interval = int((x_max - x_min) / 50)
+        x_quiver_starts = np.arange(transcript_s, transcript_e, quiver_interval)[1:-1]
+        n_quivers = len(x_quiver_starts)
+        y_quiver_starts = [transcript_line_y] * n_quivers
+        x_quiver_directs = [x_direct] * n_quivers
+        y_quiver_directs = [y_direct] * n_quivers
+        _add_quiver(ax, x_quiver_starts, y_quiver_starts, x_quiver_directs, y_quiver_directs)
+        ax.plot([transcript_s+50, transcript_e-100], 
+                [transcript_line_y, transcript_line_y], 
+                linewidth=1, color=box_color, zorder=0)
+        text_x = (transcript_s + transcript_e) / 2
+        if text_x < x_min:
+            text_x = x_min
+        if text_x > x_max:
+            text_x = x_max
+        ax.text(x=text_x, y=text_y, s=row['gene_name'], color=box_color, horizontalalignment='center', fontdict={'size':11})
+
+# cytoband tags (genomic position)
 _gtags = [s.replace('\n', '') for s in '''
 gpos1,gpos2,gpos3,gpos4,gpos5,gpos6,gpos7,gpos8,
 gpos9,gpos10,gpos11,gpos12,gpos13,gpos14,gpos15,gpos16,
@@ -37,6 +227,7 @@ gpos89,gpos90,gpos91,gpos92,gpos93,gpos94,gpos95,gpos96,
 gpos97,gpos98,gpos99,gpos100,gneg,acen,gvar,stalk
 '''.strip().split(',')]
 
+# cytoband colors
 _gcolors = [
     "#FDFDFD", "#FBFBFB", "#F8F8F8", "#F6F6F6", "#F3F3F3", "#F1F1F1", "#EEEEEE", "#ECECEC", 
     "#E9E9E9", "#E6E6E6", "#E4E4E4", "#E1E1E1", "#DFDFDF", "#DCDCDC", "#DADADA", "#D7D7D7", 
@@ -55,14 +246,17 @@ _gcolors = [
 
 
 def get_cytobands_dataframe(genome_version, _gtags=_gtags, _gcolors=_gcolors):
-    """ Return cytobands DataFrame with:
-        chromosome	start	end	name	gtag	color
-        0	1	0	2300000	p36.33	gneg	#FFFFFF
-        1	1	2300000	5400000	p36.32	gpos25	#C0C0C0
-        2	1	5400000	7200000	p36.31	gneg	#FFFFFF
-        3	1	7200000	9200000	p36.23	gpos25	#C0C0C0
-        4	1	9200000	12700000	p36.22	gneg	#FFFFFF
+    """ Return cytobands DataFrame with the following shape:
+
+        chromosome │ start     │ end       │ name   │ gtag    │ color   ║
+        1          │ 0         │ 2300000   │ p36.33 │ gneg    │ #FFFFFF ║
+        1          │ 2300000   │ 5300000   │ p36.32 │ gpos25  │ #C0C0C0 ║
+        1          │ 5300000   │ 7100000   │ p36.31 │ gneg    │ #FFFFFF ║
+        1          │ 7100000   │ 9100000   │ p36.23 │ gpos25  │ #C0C0C0 ║
+        1          │ 9100000   │ 12500000  │ p36.22 │ gneg    │ #FFFFFF ║
         ...	...	...	...	...	...	...
+
+        - genome_version: genome version, supported = {"hg19", "hg38", "grch38"}
     """
     import gzip
     import requests
@@ -161,101 +355,3 @@ def is_overlap(interval1, interval2):
     overlap = (start2 < end1) and (start1 < end2)
     return overlap
 
-def calc_transcript_offset(transcript_start_ends, offset=0):
-    curr_start, curr_end = transcript_start_ends[-1]
-    prev_start, prev_end = transcript_start_ends[-2]
-    if is_overlap((curr_start, curr_end), (prev_start, prev_end)):
-        offset += 1
-    return offset
-
-def add_quiver(ax, x_quiver_starts, y_quiver_starts, x_quiver_directs, y_quiver_directs, box_color='#090A76'):
-    ax.quiver(
-        x_quiver_starts, y_quiver_starts, x_quiver_directs, y_quiver_directs,
-        color=box_color,
-        width=0.001, 
-        headwidth=10, 
-        headaxislength=3,
-        headlength=6, 
-        pivot='tip',
-        scale=200,
-    )
-
-def add_transcript_rectangles(ax, row, transcript_s, transcript_e, y_offset, strand, add_annot=False):
-    """ Add one exon rectangle, and if add_annot add central line, quivers, and gene_name text
-    """
-    box_color = '#090A76'
-    strand_color = 'blue'
-    x_direct, y_direct = -1, 0
-    if strand == '+': 
-        x_direct = 1
-        strand_color = 'red'
-    x_min, x_max = ax.get_xlim()
-    y_offset_factor = 3
-    y_offset *= y_offset_factor
-    exon_s, exon_e = row['Start'], row['End']
-    feature = row['Feature']
-    rect_y, rect_height = 1+y_offset, 1
-    transcript_line_y = rect_y + 0.5 * rect_height
-    text_y = rect_y - rect_height + 3
-    if feature == 'CDS':
-        rect_y = 0.5 + y_offset
-        rect_height = 2
-    exon_len = exon_e - exon_s
-    rect_x = max(exon_s, x_min)
-    
-    # for each exon / CDS
-    ax.add_patch(matplotlib.patches.Rectangle((rect_x, rect_y), exon_len, rect_height, 
-                                                linewidth=1, edgecolor=strand_color, color=box_color, zorder=1))
-    if add_annot: # done only once 
-        # add central line
-        ax.plot([transcript_s+50, transcript_e-100], [transcript_line_y, transcript_line_y], 
-                linewidth=1, color=box_color, zorder=0)
-
-        # add gene orientation quivers
-        quiver_interval = int((x_max - x_min) / 60)
-        x_quiver_starts = np.arange(transcript_s, transcript_e, quiver_interval)[1:-1]
-        n_quivers = len(x_quiver_starts)
-        y_quiver_starts = [transcript_line_y] * n_quivers
-        x_quiver_directs = [x_direct] * n_quivers
-        y_quiver_directs = [y_direct] * n_quivers
-        add_quiver(ax, x_quiver_starts, y_quiver_starts, x_quiver_directs, y_quiver_directs, box_color)
-
-        # add gene name text
-        text_x = (transcript_s + transcript_e) / 2
-        if text_x < x_min: text_x = x_min
-        if text_x > x_max: text_x = x_max
-        ax.text(x=text_x, y=text_y, s=row['gene_name'], color=box_color, ha='center', fontdict={'size':11})
-
-def plot_gene_annotations(ax, gene_exons, return_offset=True):
-    """ Plot UCSC-like gene annotations
-    - ax: pyplot Axes
-    - gene_exons: DataFrame of pygtf 'exon' and 'CDS' for the gene
-    - return_offset: if True return gene2offset dict
-
-    gene_exons is created by:
-    [1] loading a gtf with pygtf
-    [2] filtering gtf with a gene_name
-    [3] selecting the transcript with largest transcript start-end span
-    [4] filtering gtf from [2] with the transcript ID
-
-    Returns:
-    - gene2offset: dict[gene_name] = 
-    """
-    gene2offset = {}
-    transcript_start_ends = []
-    transcript_offset = 0
-    for gene, exons in gene_exons.items():
-        strands = exons['Strand'].unique()
-        assert len(strands) == 1, strands
-        strand = strands[0]
-        transcript_s, transcript_e = exons['Start'].min(), exons['End'].max()
-        transcript_start_ends.append((transcript_s, transcript_e))
-        if len(transcript_start_ends) >= 2:
-            transcript_offset = calc_transcript_offset(transcript_start_ends, offset=transcript_offset)
-        gene2offset[gene] = transcript_offset
-        for i, (_, row) in enumerate(exons.iterrows()):
-            add_annot = (i==0)
-            add_transcript_rectangles(ax, row, transcript_s, transcript_e, transcript_offset, strand, add_annot=add_annot) # add rectangles and a line
-            
-    if return_offset:
-        return gene2offset
